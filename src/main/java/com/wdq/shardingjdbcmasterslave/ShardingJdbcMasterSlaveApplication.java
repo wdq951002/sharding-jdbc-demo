464fc070b5269383f827df75bedcbcc74d5aca50
Weng Diquan <BG405275@800best.net>
2019-10-12 16:29:39 +0800

    first commit

---
+++ src/main/java/com/wdq/shardingjdbcmasterslave/ShardingJdbcMasterSlaveApplication.java
@@ -0,0 +1,13 @@
+package com.wdq.shardingjdbcmasterslave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingJdbcMasterSlaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingJdbcMasterSlaveApplication.class, args);
+    }
+
+}
